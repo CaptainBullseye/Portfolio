2d988a4eeb89745338b7813cad622a3e81c877eb
Vulders,Dennis D <d.vulders@student.fontys.nl>
2019-10-01 19:48:27 +0200

    New node app voor portfolio

---
+++ portfolio/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const toolbar = props => (
+    <header>
+        <nav>
+            <div></div>
+            <div><a href="/">LOGO</a></div>
+            <div>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Page2</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default toolbar;
