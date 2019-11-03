18b84acfa7e4e81b03b0aa87ef0ab87894a1d7f1
DennisVulders <d.vulders@student.fontys.nl>
2019-11-03 18:37:46 +0100

    Added div style

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -50,11 +50,13 @@ class App extends Component {
                 <ContentSlider />
                 {/* <ProgessBar /> */}
                 <Fragment>
-                    <button onClick={() => this.setState({ percentage: this.state.percentage + 10 })} >Increase</button>
-                    {/* moet uiteindelijk de 'start' knop worden */}
-                    <button onClick={() => this.setState({ percentage: this.state.percentage - 10 })} >Decrease</button>
-                    {/* moet uiteindelijk de 'reset' knop worden */}
-                    <ProgBar percentage={this.state.percentage} />
+                    <div style={{height:'300px', width: '100%', paddingTop: '100px'}}>
+                        <button onClick={() => this.setState({ percentage: this.state.percentage + 10 })} >Increase</button>
+                        {/* moet uiteindelijk de 'start' knop worden */}
+                        <button onClick={() => this.setState({ percentage: this.state.percentage - 10 })} >Decrease</button>
+                        {/* moet uiteindelijk de 'reset' knop worden */}
+                        <ProgBar percentage={this.state.percentage} />
+                    </div>
                 </Fragment>
                 
 
